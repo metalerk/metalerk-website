85e64c7f36936787cb4a021ade2fbbfb7c9b9141
luis <rodriguezjluis0@gmail.com>
2016-09-13 06:06:53 -0500

    db engine updated

--- metalerk/settings.py
+++ metalerk/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'metalerk.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'metweb',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': 3306,
     }
 }
 
